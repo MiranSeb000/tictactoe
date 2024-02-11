3e96b8a195b6c10884da240d47fec0ecd1961cbd
Peter-Hafner <hafnerp2@wwu.edu>
2024-02-10 20:54:32 -0800

    added game loop structure and evaluation equation

--- Game.java
+++ Game.java
@@ -8,8 +8,38 @@ public class Game {
         Scanner scan = new Scanner(System.in);
         Board board = new Board();
         System.out.print(border);
-        int opp = getAIOpp(scan);
+
+        // get AI's opponent
+        int AIOpp = getAIOpp(scan);
         
+        // randomly decide who goes first
+        if (Math.random()<0.5){
+            System.out.println("AI goes first.");
+        } else {
+            System.out.println("You go first.");
+        }
+
+        int row, col;
+        // game loop
+        while(!board.checkWin()){
+            // player 1
+            System.out.println(border);
+            board.printBoard();
+            System.out.println("Player 1's turn...");
+            System.out.print("Enter row[0 to 2]: ");
+            row = scan.nextInt();
+            System.out.print("Enter col [0 to 2]: ");
+            col = scan.nextInt();
+
+            // player 2
+            System.out.println(border);
+            board.printBoard();
+            System.out.println("Player 2's turn...");
+            System.out.print("Enter row[0 to 2]: ");
+            row = scan.nextInt();
+            System.out.print("Enter col [0 to 2]: ");
+            col = scan.nextInt();
+        }
         System.out.println(border);
 
 
