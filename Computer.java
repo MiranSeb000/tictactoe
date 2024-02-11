75dc61c33f0c65e5068c4190e5b45d00ad2f9e69
miranseb <mirands2@wwu.edu>
2024-02-11 14:54:03 -0800

    Search function now plays game

--- Computer.java
+++ Computer.java
@@ -6,33 +6,41 @@ public class Computer extends Player{
         super(piece);
     }
 
-    public int search(Board board, int depth){
+    public int searchAndPlay(Board board, int depth, Boolean isRoot){
         
         if (depth == 0) {
             return board.evaluate();
         }
 
-        LinkedList<Move> moves = generateMoves(piece, board);
+        LinkedList<Move> moves = generateMoves(board);
         if (moves.size() == 0) {
             return 0; //FIGURE OUT BEST PLAY HERE DRAW?
         }
         
         int bestEval;
+        Move bestMove;
         if (board.pieceToMove == 'X') bestEval = Integer.MIN_VALUE;
         else bestEval = Integer.MAX_VALUE;
         
         for (Move move : moves) {
             board.makeMove(move);
-            int eval = search(board, depth - 1);
-            if (board.pieceToMove == 'X') bestEval = Math.max(eval, bestEval);
-            else bestEval = Math.min(eval, bestEval);
+            int eval = searchAndPlay(board, depth - 1, false);
+
+            if (board.pieceToMove == 'X'){
+                bestEval = Math.max(eval, bestEval);
+                if (bestEval == eval && isRoot) bestMove = move;
+            } 
+            else {
+                bestEval = Math.min(eval, bestEval);
+                if (bestEval == eval && isRoot) bestMove = move;
+            }
             board.unmakeMove(move);
         }
-
+        if (isRoot) board.makeMove(bestMove);
         return bestEval;
     }
 
-    private LinkedList<Move> generateMoves (char piece, Board board) {
+    private LinkedList<Move> generateMoves (Board board) {
         LinkedList<Move> moves = new LinkedList<Move> ();
         for (int row = 0; row < 3; row++){
             for (int col = 0; col < 3; col++){
