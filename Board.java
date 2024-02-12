50f1ac629820a73b768e6cda2e45a7881d8ee8f1
miranseb <mirands2@wwu.edu>
2024-02-11 23:14:27 -0800

    Eval condensed

--- Board.java
+++ Board.java
@@ -44,97 +44,74 @@ public class Board {
         }
     }
 
-    /* Returns integer value of current position in form 3x_2(s)+x_1(s)-(3O_2(s)+O_1(s)) */
+    /* Returns the value of this row/col/diagonal */
+    private int checkTriplet(int Xs, int Os) {
+        if (Xs == 3) {
+            isWon = true;
+            return Integer.MAX_VALUE;
+        } else if (Os == 3) {
+            isWon = true;
+            return Integer.MIN_VALUE;
+        } 
+        else if (Xs == 2 && Os == 0) return 3;
+        else if (Xs == 1 && Os == 0) return 1;
+        else if (Os == 2 && Xs == 0) return -3;
+        else if (Os == 1 && Xs == 0) return -1;
+        return 0;
+    }
+
+    /* Returns integer value of current position in form 3(X_2)+x_1-(3(O_2)+O_1) */
     public int evaluate() {
 
+        /* Set isWon to make sure this evaluation is fresh */
+        isWon = false;
+
         /* Variables used in the heuristic */
-        int Xs = 0, X2 = 0, X1 = 0, Os = 0, O1 = 0, O2 = 0;
+        int Xs = 0, Os = 0, evaluation = 0, tripletValue;
 
         /* Check rows */
         for(int row = 0; row <3; row++) {
             Xs=0; Os=0;
             for (int col = 0; col < 3; col++) {
-                if(board[row][col] == 'X'){
-                    Xs++;
-                } else if (board[row][col] == 'O'){
-                    Os++;
-                }
+                if(board[row][col] == 'X')Xs++;
+                else if (board[row][col] == 'O')Os++;
             }
-            if (Xs == 3) {
-                isWon = true;
-                return Integer.MAX_VALUE;
-            } else if (Os == 3) {
-                isWon = true;
-                return Integer.MIN_VALUE;
-            } 
-            else if (Xs == 2 && Os == 0) X2++;
-            else if (Xs == 1 && Os == 0) X1++;
-            else if (Os == 2 && Xs == 0) O2++;
-            else if (Os == 1 && Xs == 0) O1++;
+            tripletValue = checkTriplet(Xs, Os);
+            if (isWon) return tripletValue;
+            evaluation += tripletValue;
         }
 
         /* Check cols */
         for(int col = 0; col <3; col++) {
             Xs=0; Os=0;
             for (int row = 0; row < 3; row++) {
-                if(board[row][col] == 'X'){
-                    Xs++;
-                } else if (board[row][col] == 'O'){
-                    Os++;
-                }
+                if(board[row][col] == 'X') Xs++;
+                else if (board[row][col] == 'O') Os++;
             }
-            if (Xs == 3) {
-                isWon = true;
-                return Integer.MAX_VALUE;
-            } else if (Os == 3) {
-                isWon = true;
-                return Integer.MIN_VALUE;
-            }
-            else if (Xs == 2 && Os == 0) X2++;
-            else if (Xs == 1 && Os == 0) X1++;
-            else if (Os == 2 && Xs == 0) O2++;
-            else if (Os == 1 && Xs == 0) O1++;
+            tripletValue = checkTriplet(Xs, Os);
+            if (isWon) return tripletValue;
+            evaluation += tripletValue;
         }
 
         /* Check diagonals */
         Xs=0; Os=0;
         for (int diag = 0; diag < 3; diag++){
-            if(board[diag][diag] == 'X') {
-                Xs++;
-            } else if (board[diag][diag] == 'O') {
-                Os++;
-            }
+            if(board[diag][diag] == 'X') Xs++;
+            else if (board[diag][diag] == 'O') Os++;
         }
-        if (Xs == 3) {
-            isWon = true;
-            return Integer.MAX_VALUE;
-        } else if (Os == 3) {
-            isWon = true;
-            return Integer.MIN_VALUE;
-        }
-        else if (Xs == 2 && Os == 0) X2++;
-        else if (Xs == 1 && Os == 0) X1++;
-        else if (Os == 2 && Xs == 0) O2++;
-        else if (Os == 1 && Xs == 0) O1++;
+        tripletValue = checkTriplet(Xs, Os);
+        if (isWon) return tripletValue;
+        evaluation += tripletValue;
         
         Xs = 0; Os = 0;
         for (int diag = 0; diag < 3; diag++){
             if(board[diag][2-diag] == 'X') Xs++;
             else if (board[diag][2-diag] == 'O') Os++;
         }
-        if (Xs == 3) {
-            isWon = true;
-            return Integer.MAX_VALUE;
-        } else if (Os == 3) {
-            isWon = true;
-            return Integer.MIN_VALUE;
-        }
-        else if (Xs == 2 && Os == 0) X2++;
-        else if (Xs == 1 && Os == 0) X1++;
-        else if (Os == 2 && Xs == 0) O2++;
-        else if (Os == 1 && Xs == 0) O1++;
+        tripletValue = checkTriplet(Xs, Os);
+        if (isWon) return tripletValue;
+        evaluation += tripletValue;
         
-        isWon = false;
-        return 3*X2+X1-(3*O2+O1);
+        return evaluation;
     }
 }
